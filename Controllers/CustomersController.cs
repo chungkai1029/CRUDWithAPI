c5dad95a1ea93224b995ec5a2533699bf56597bf
chungkai1029 <chungkai1029@gmail.com>
2023-12-03 11:58:29 +0800

    Add models and controllers with the column name of Northwind.dbo.Customers

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseFun.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public void GetCustomersData()
+        {
+            
+        }
+
+        [HttpGet(template: "{id}")]
+        public void GetCustomerDataById(int id)
+        {
+
+        }
+
+        [HttpPost]
+        public void AddCustomersData()
+        {
+
+        }
+
+        [HttpPut(template: "{id}")]
+        public void UpdateAllOfCustomerData(int id)
+        {
+
+        }
+
+        [HttpPatch(template: "{id}")]
+        public void UpdatePartOfCustomerData(int id)
+        {
+
+        }
+
+        [HttpDelete(template: "{id}")]
+        public void DeleteCustomerData(int id)
+        {
+
+        }
+    }
+}
